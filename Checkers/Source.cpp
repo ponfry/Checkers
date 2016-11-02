b0b7773a9e0fc538f3d7925e5c0293ddb13509c3
отделенов павел <mr.otdelenov@mail.ru>
2016-11-02 17:57:03 +0300

    fix

--- Checkers/Source.cpp
+++ Checkers/Source.cpp
@@ -2,6 +2,7 @@
 #include "glut.h" 
 #include <iostream>
 #include "Helper.h"
+#include "il.h"
 using namespace std;
 
 struct Size
@@ -10,10 +11,65 @@ struct Size
 	int height;
 }size;
 
+float coordinateX = 0;
+float coordinateY = 0;
+
+struct Characteristc
+{
+	ILubyte* texture;
+	ILuint width;
+	ILuint height;
+	ILuint type;
+	ILuint format;
+};
+
+ILuint images;
+Characteristc characteristics;
+unsigned int* textures;
+
+void TextureInitialization()
+{
+	textures = new unsigned int;
+	ilGenImages(1, &images);
+
+	ilBindImage(images);
+	ilLoadImage(L"texture.jpg");
+	characteristics.texture = ilGetData();
+	characteristics.width = ilGetInteger(IL_IMAGE_WIDTH);
+	characteristics.height = ilGetInteger(IL_IMAGE_HEIGHT);
+	characteristics.type = ilGetInteger(IL_IMAGE_TYPE);
+	characteristics.format = ilGetInteger(IL_IMAGE_FORMAT);
+	glGenTextures(1, textures);
+}
+
 void display()
 {
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-	glViewport(0, 0, WindowSize::Weigth, WindowSize::Heigth);
+	
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, textures[0]);
+	glEnable(GL_TEXTURE_2D);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, characteristics.width, characteristics.height, 0, characteristics.format, characteristics.type, characteristics.texture);
+	glBegin(GL_TRIANGLE_FAN);
+	glTexCoord2f(coordinateX, coordinateY);
+	glVertex3f(-1.0f, 1.0f, 0.0f);
+	glTexCoord2f(coordinateX + 0.5, coordinateY);
+	glVertex3f(1.0f, 1.0f, 0.0f);
+	glTexCoord2f(coordinateX + 0.5, coordinateY + 0.5);
+	glVertex3f(1.0f, -1.0f, 0.0f);
+	glTexCoord2f(coordinateX, coordinateY + 0.5);
+	glVertex3f(-1.0f, -1.0f, 0.0f);
+	glEnd();
+
+
+	
+	/*glViewport(0, 0, WindowSize::Weigth, WindowSize::Heigth);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1.0, 1.0, 1.0);
 	glBegin(GL_TRIANGLE_STRIP);
@@ -22,7 +78,7 @@ void display()
 	glVertex2f(1.0f, -1.0f);
 	glVertex2f(1.0f, 1.0f);
 	glEnd();
-
+*/
 	glColor3f(0.0, 0.0, 1.0);
 	glBegin(GL_TRIANGLE_STRIP);
 	glVertex2f(-0.31f, -0.615f);
@@ -30,10 +86,10 @@ void display()
 	glVertex2f(0.31f, -0.615f);
 	glVertex2f(0.31f, 0.615f);
 	glEnd();
-	
+
 	glViewport(350, 100, 300, 300);
 
-	
+
 	glColor3f(1.0, 1.0, 1.0);
 	glBegin(GL_TRIANGLE_STRIP);
 	glVertex2f(-1.0f, -1.0f);
@@ -41,7 +97,7 @@ void display()
 	glVertex2f(1.0f, -1.0f);
 	glVertex2f(1.0f, 1.0f);
 	glEnd();
-	
+
 
 	int count = 0;
 	float x = -0.75, y = -1.0;
@@ -70,19 +126,48 @@ void display()
 		}
 		y += 0.25f;
 	}
-
-
 	glutSwapBuffers();
 }
+
+void display1()
+{
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, textures[0]);
+	glEnable(GL_TEXTURE_2D);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, characteristics.width, characteristics.height, 0, characteristics.format, characteristics.type, characteristics.texture);
+	glBegin(GL_TRIANGLE_FAN);
+	glTexCoord2f(coordinateX, coordinateY);
+	glVertex3f(-1.0f, 1.0f, 0.0f);
+	glTexCoord2f(coordinateX + 0.5, coordinateY);
+	glVertex3f(1.0f, 1.0f, 0.0f);
+	glTexCoord2f(coordinateX + 0.5, coordinateY + 0.5);
+	glVertex3f(1.0f, -1.0f, 0.0f);
+	glTexCoord2f(coordinateX, coordinateY + 0.5);
+	glVertex3f(-1.0f, -1.0f, 0.0f);
+	glEnd();
+	glutSwapBuffers();
+}
+
 
 void Init(int argc, char* argv[])
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
 	glutInitWindowPosition(100, 100);
+	
 	glutInitWindowSize(WindowSize::Weigth, WindowSize::Heigth);
+	
 	glutCreateWindow("Test Window");
+	ilInit(); //Инициализация библиотеки
+	TextureInitialization();
 }
+
+
 
 void main(int argc, char* argv[])
 {
